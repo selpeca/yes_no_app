10857723a448f175a2125705c7f0116323e9ae6f
Sergio Elías Pérez Camargo <ser.per.eli@gmail.com>
2026-07-16 22:44:00 -0500

    feat: add HerMessageBubble widget and update message display logic

--- lib/presentation/screens/chat/chat_screen.dart
+++ lib/presentation/screens/chat/chat_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:yes_no_app/presentation/widgets/chat/her_message_bubble.dart';
 import 'package:yes_no_app/presentation/widgets/chat/my_message_bubble.dart';
 
 class ChatScreen extends StatelessWidget {
@@ -34,7 +35,11 @@ class _ChatView extends StatelessWidget {
           children: [
             Expanded(child: ListView.builder(
               itemCount: 100,
-              itemBuilder: (context, index) => MyMessageBubble(),
+              itemBuilder: (context, index) {
+                return (index % 2 == 0)
+                ? const MyMessageBubble()
+                : const HerMessageBubble();
+              },
             )),
             Text('Hola mundo'),
           ],
---
+++ lib/presentation/widgets/chat/her_message_bubble.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+class HerMessageBubble extends StatelessWidget {
+
+  const HerMessageBubble({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Container(
+            decoration: BoxDecoration(
+              color: colors.secondary,
+              borderRadius: BorderRadius.circular(20)
+            ),
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+              child: Text('Hola mundo', style: TextStyle(color: Colors.white),),
+            )
+        ),
+        const SizedBox(height: 5,),
+        _ImageBubble(),
+        const SizedBox(height: 10,),
+      ],
+    );
+  }
+}
+
+class _ImageBubble extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+
+    final size = MediaQuery.of(context).size;
+    final width = size.width * 0.7;
+    
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(20),
+      child: Image.network(
+        'https://yesno.wtf/assets/no/32-b62f1f8058c1d7f06c528319faccfb38.gif',
+        width: width,
+        height: 150,
+        fit: BoxFit.cover,
+      ),
+    );
+  }
+}
