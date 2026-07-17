92f223224dd01b090272d95bed656c8ff5373cf5
Sergio Elías Pérez Camargo <ser.per.eli@gmail.com>
2026-07-16 22:26:55 -0500

    feat: reorganize chat screen structure and add message bubble widget

--- lib/presentation/screens/chat/chat_screen.dart
+++ lib/presentation/screens/chat/chat_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:yes_no_app/presentation/widgets/chat/my_message_bubble.dart';
 
 class ChatScreen extends StatelessWidget {
   const ChatScreen({super.key});
@@ -33,7 +34,7 @@ class _ChatView extends StatelessWidget {
           children: [
             Expanded(child: ListView.builder(
               itemCount: 100,
-              itemBuilder: (context, index) => Text('Hola soy Sergio $index'),
+              itemBuilder: (context, index) => MyMessageBubble(),
             )),
             Text('Hola mundo'),
           ],
