fb71a54c87264980fb4b9c88d3d5880be0a88b09
Sergio Elías Pérez Camargo <ser.per.eli@gmail.com>
2026-07-17 23:07:43 -0500

    feat: refactor MessageFieldBox to use TextFormField with improved input decoration

--- lib/presentation/widgets/shared/message_field_box.dart
+++ lib/presentation/widgets/shared/message_field_box.dart
@@ -6,26 +6,36 @@ class MessageFieldBox extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.symmetric(horizontal: 10),
-      padding: const EdgeInsets.symmetric(horizontal: 20),
-      height: 60,
-      decoration: BoxDecoration(
-        color: Colors.grey[200],
-        borderRadius: BorderRadius.circular(100)
-      ),
-      child: Row(
-        children: [
-          const Expanded(
-            child: TextField(
-              decoration: InputDecoration(
-                hintText: 'Escribe un mensaje',
-                border: InputBorder.none
-              ),
-            ),
-          ),
-          ],
-      ),
+
+    final outlineInputBorder = OutlineInputBorder(
+      borderSide: const BorderSide(color: Colors.transparent),
+      borderRadius: BorderRadius.circular(20),
     );
+
+    final inputDecoration = InputDecoration(
+      enabledBorder: outlineInputBorder,
+      focusedBorder: outlineInputBorder,
+      filled: true,
+      suffixIcon: IconButton(
+        icon: const Icon(Icons.send_outlined),
+        onPressed: () {
+          // Aquí puedes agregar la lógica para enviar el mensaje
+          print('Valor caja de texto  ');
+        },
+      ),
+      hintText: 'Escribe un mensaje',
+    );
+    
+    return TextFormField(
+        decoration: inputDecoration,
+        onFieldSubmitted: (value) {
+          // Aquí puedes agregar la lógica para enviar el mensaje
+          print('Submitted value: $value');
+        },
+        onChanged: (value) {
+          // Aquí puedes agregar la lógica para manejar el cambio de texto
+          print('Changed value: $value');
+        },
+      );
   }
 }
